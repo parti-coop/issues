838587ea9a677ddebcab9e9016e4e6b6c6bda6cf
Ohyeon <rest515@gmail.com>
2020-04-17 13:32:39 +0900

    사용자의 이름은 유일해야 합니다

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   has_many :articles
   has_many :comments
