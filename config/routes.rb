b2a44e42cb5baa0752f26c2db53a5f9d3005f713
rest515 <rest515@gmail.com>
2020-05-07 16:08:40 +0900

    로고를 추가합니다

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :users
   resources :comments, only: [:create, :destroy]
 
+  get 'pages/about', as: 'about'
+
   if Rails.env.development?
     require 'sidekiq/web'
     require 'sidekiq/cron/web'
