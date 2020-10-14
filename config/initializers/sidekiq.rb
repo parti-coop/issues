cf7f8acb59f476a08889392c4c35853dc6bdc1db
dalikim <dalikim@parti.xyz>
2020-10-14 16:42:26 +0900

    chore: sidekiq 설정합니다

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,15 @@ if !ENV['SIDEKIQ'] and (Rails.env.development? or Rails.env.test?)
   require 'sidekiq/testing'
   Sidekiq::Testing.inline!
 else
+  redis_config = {
+    url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}",
+    namespace: "issues:#{Rails.env}"
+  }
   Sidekiq.configure_server do |config|
-    config.redis = {namespace: "issues:#{Rails.env}"}
+    config.redis = redis_config
   end
   Sidekiq.configure_client do |config|
-    config.redis = {namespace: "issues:#{Rails.env}"}
+    config.redis = redis_config
   end
 
   schedule_file = "config/schedule.yml"
